000dff0954a83b33eb713694449158686002eef9
ictbeheer <ictbeheer@siw.nl>
2016-01-10 20:48:27 +0100

    Wordpress 4.4: Mixed content bij afbeeldingen

--- includes/siw-system.php
+++ includes/siw-system.php
@@ -133,7 +133,18 @@ function siw_cmb_meta_box_url( $cmb_url ){
 	$cmb_url = str_replace("http://", "//", $cmb_url);
     return $cmb_url;
 }
-     
+
+
+//afbeelding in admin over ssl i.v.m. mixed content
+add_filter('wp_calculate_image_srcset', 'siw_set_image_srcset_to_ssl');
+function siw_set_image_srcset_to_ssl($sources) {
+	if ( is_ssl() ){
+		foreach($sources as $size => &$source){
+			$source['url'] = set_url_scheme( $source['url'] ,'https');
+		}
+	}
+	return $sources;
+}
 
 /*
 */
