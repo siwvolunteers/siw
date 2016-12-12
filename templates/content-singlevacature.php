76c6fb1b2cc052da69d5b530e1b2f1541fad1035
ICT beheer <ictbeheer@siw.nl>
2016-12-12 20:55:38 +0100

    Vacature: escaping

--- templates/content-singlevacature.php
+++ templates/content-singlevacature.php
@@ -20,10 +20,10 @@
 	$meervoud					= get_post_meta( $post->ID, 'siw_vacature_meervoud', true );
 	
 	$content = '[accordion]';
-	$content .= '[pane title="Wie zijn wij?"]' . wpautop( $missie ) . wpautop( $wie_zijn_wij ) . '[/pane]';
-	$content .= '[pane title="Wie ben jij?"]' . wpautop( $wie_ben_jij ) . '[/pane]';
-	$content .= '[pane title="Wat ga je doen?"]' . wpautop( $wat_ga_je_doen ) . '[/pane]';
-	$content .= '[pane title="Wat bieden wij jou?"]' . wpautop( $wat_bieden_wij_jou ) . '[/pane]';
+	$content .= '[pane title="Wie zijn wij?"]' . wp_kses_post( wpautop( $missie ) . wpautop( $wie_zijn_wij ) ) . '[/pane]';
+	$content .= '[pane title="Wie ben jij?"]' . wp_kses_post( wpautop( $wie_ben_jij ) ). '[/pane]';
+	$content .= '[pane title="Wat ga je doen?"]' . wp_kses_post( wpautop( $wat_ga_je_doen ) ) . '[/pane]';
+	$content .= '[pane title="Wat bieden wij jou?"]' . wp_kses_post( wpautop( $wat_bieden_wij_jou ) ) . '[/pane]';
 	$content .= '[/accordion]';
 ?>
 	
@@ -38,22 +38,23 @@
 				<div class="row">
 					<div class="col-md-7">
 						<h3 class="job-heading">Vacature</h3>
-						<p><?php echo get_the_excerpt(); ?> <?php echo 'Wij zijn op zoek naar', ( $meervoud ?':':' een:');?></p>
+						<?php the_excerpt(); ?>
+						<p><?php echo 'Wij zijn op zoek naar', ( $meervoud ?':':' een:');?></p>
 						<h5 class="job-title"><?php the_title();?></h5>
-						<?php echo ($inleiding ? wpautop( $inleiding ) : ''  ); ?>
+						<?php echo ($inleiding ? wp_kses_post( wpautop( $inleiding ) ) : ''  ); ?>
 						<?php echo do_shortcode( $content );?>
 					</div>
 					<div class="col-md-5">
 					<?php if( $deadline_ts >= time() ):?>
 						<h3>Meer weten?</h3>
 						<p>Voor meer informatie kun je contact opnemen met:<br />
-						<?php echo $contactpersoon_naam, (( $contactpersoon_functie )?' ('. $contactpersoon_functie . '), ':', '), '<a class="email" href="mailto:', antispambot( $contactpersoon_email ), '">', antispambot( $contactpersoon_email ),'</a>';?>
+						<?php echo $contactpersoon_naam, (( $contactpersoon_functie )?' ('. esc_html( $contactpersoon_functie ) . '), ':', '), '<a class="email" href="mailto:', antispambot( $contactpersoon_email ), '">', antispambot( $contactpersoon_email ),'</a>';?>
 						</p>
 						<h3>Solliciteren?</h3>
 						<p>
-						<?php echo 'Je motivatie met cv kun je uiterlijk ', $deadline, ' sturen naar ', $solliciteren_naam, (( $solliciteren_functie )?' (' . $solliciteren_functie . ')':''), 
-						', <a class="email" href="mailto:', antispambot( $solliciteren_email ),'">', antispambot( $solliciteren_email ), '</a>, onder vermelding van "Sollicitatie ', the_title(),'".</p>';?>
-						<p><?php echo $gesprekken;?></p>
+						<?php echo 'Je motivatie met cv kun je uiterlijk ', $deadline, ' sturen naar ', $solliciteren_naam, (( $solliciteren_functie )?' (' . esc_html($solliciteren_functie) . ')':''), 
+						', <a class="email" href="mailto:', antispambot( $solliciteren_email ),'">', antispambot( $solliciteren_email ), '</a>, onder vermelding van "Sollicitatie ', the_title_attribute(),'".</p>';?>
+						<p><?php echo esc_html( $gesprekken );?></p>
 						<?php else: ?>
 						<h5>Het is helaas niet meer mogelijk om op deze vacature te reageren</h5>
 						<?php endif; ?>
