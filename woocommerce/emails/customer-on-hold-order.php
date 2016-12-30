3c6dfe124abb4d14d83e0ab95922dcda19b312e0
ICT beheer <ictbeheer@siw.nl>
2016-12-30 21:14:00 +0100

    GP: Bevestigingsmail bij zelf overmaken
    Fixes #95

---
+++ woocommerce/emails/customer-on-hold-order.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Customer on-hold order email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-processing-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.5.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+//gegevens aanmelding
+$application_number = $order->get_order_number();
+//ondertekening
+$signature = siw_get_mail_signature_name('aanmelding_groepsproject');
+$iban = siw_get_general_information('iban');
+
+/**
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<font style="font-family:'Open Sans', Verdana, normal; color:#444; font-size:14px; ">
+<p>Beste <?php echo $order->billing_first_name ?>,<br/><br/>
+Heel erg bedankt voor je aanmelding voor een vrijwilligersproject via SIW! We doen ons best om ervoor te zorgen dat dit voor jou een onvergetelijke ervaring wordt!<br/><br/>Je inschrijving wordt pas in behandeling genomen als we je betaling ontvangen hebben.<br/><br/>
+Je kunt je betaling overmaken naar <?php echo esc_html( $iban );?> o.v.v. je aanmeldnummer (<?php echo esc_html( $application_number );?>).<br/><br/>
+Als je nog vragen hebt, aarzel dan niet om contact met ons op te nemen.<br/><br/>
+Met vriendelijke groet,<br/><br/>
+<?php echo esc_html( $signature )?> 
+</p>
+</font>
+
+<?php siw_wc_email_show_project_details( $order, $application_number );?>
+
+<?php siw_wc_email_show_application_details( $order );?>
+
+<?php
+/**
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+do_action( 'woocommerce_email_footer', $email );
