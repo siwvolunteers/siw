3609ffa2c9263c5573af1172f4629b9ca55d42f0
ICT beheer <ictbeheer@siw.nl>
2017-06-16 23:10:27 +0200

    Update agenda template

--- templates/content-singleagenda.php
+++ templates/content-singleagenda.php
@@ -17,23 +17,31 @@
 			<div class="postclass">
 				<header class="agenda-header">
 					<h1><?php the_title();?></h1>
-					<h2><?php echo esc_html( $event_data['duration'] );?></h2>
+					<h5><?php echo esc_html( $event_data['duration'] );?></h5>
+					<hr>
 				</header>
 				<div class="row">
-					<div class="col-md-10">
+					<div class="col-md-7">
 						<?php echo wp_kses_post( wpautop( $event_data['description'] ) ); ?>
+					</div>
+					<div class="col-md-5">
+						<?php if ( ! empty( $event_data['highlight_quote'] ) ): ?>
+						<div class="pullquote-center agenda-quote">
+							<?php echo esc_html( $event_data['highlight_quote'] );?>
+						</div>
+						<?php endif ?>
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-6">
-						<?php if( $event_data['program'] ){?>
+					<div class="col-md-7">
+						<?php if ( $event_data['program'] ) {?>
 						<h3><?php esc_html_e('Programma', 'siw');?></h3>
 						<div class="row">
 						<?php
 						foreach ( (array) $event_data['program'] as $key => $item ) {
 						?>
 							<div class="col-xs-3">
-								<p><b><?php echo esc_html( date("H:i", strtotime( $item['starttijd'])) .  '&nbsp;-&nbsp;' . date("H:i", strtotime( $item['eindtijd'] ) ) );?></b></p>
+								<p><b><?php echo esc_html( date('H:i', strtotime( $item['starttijd'])) .  '&nbsp;-&nbsp;' . date('H:i', strtotime( $item['eindtijd'] ) ) );?></b></p>
 							</div>
 							<div class="col-xs-9">
 								<?php echo wp_kses_post( wpautop( $item['omschrijving'] ) );?>
@@ -52,7 +60,7 @@
 						</p>
 						<?php echo do_shortcode( $location_map );?>
 					</div>
-					<div class="col-md-6">
+					<div class="col-md-5">
 						<h3><?php esc_html_e('Aanmelden', 'siw');?></h3>
 						<?php if( $event_data['end_date'] > date("Y-m-d") ):?>
 						<?php if ('formulier' == $event_data['application'] ){
@@ -60,12 +68,12 @@
 								echo do_shortcode( '[vfb id=' . $vfb_form_id . ']' );
 							}
 							else{
-								echo wp_kses_post( wpautop( ($text_after_hide_cd_form )) ? $text_after_hide_cd_form : __('Het is helaas niet meer mogelijk om je aan te melden.','siw'));
+								echo wp_kses_post(  ( $event_data['text_after_hide_form'] ) ? wpautop( $event_data['text_after_hide_form'] ) : __('Het is helaas niet meer mogelijk om je aan te melden.','siw') );
 							}
-						}else{?>
-							<?php echo wp_kses_post( wpautop( $event_data['application_explanation'] )); ?>
+						} else {?>
+							<?php echo wp_kses_post( wpautop( $event_data['application_explanation'] ) ); ?>
 						<?php
-							if ( $event_data['application_link_url'] ){?>
+							if ( $event_data['application_link_url'] ) {?>
 							<a href="<?php echo esc_url( $event_data['application_link_url'] );?>" target="_blank"><?php echo ( $event_data['application_link_text'] ) ? esc_html( $event_data['application_link_text'] ) : esc_html( $event_data['$application_link_url'] )?> <i class="kt-icon-newtab"></i></a>
 
 						<?php
