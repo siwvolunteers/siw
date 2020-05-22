af9e1ea5e939f3f679f4754a92f65eb95cdf6202
ICT beheer <ictbeheer@siw.nl>
2020-05-22 22:47:05 +0200

    Switch naar GeneratePress

---
+++ archive-siw_event.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+	<div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+		<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+			<?php
+			/**
+			 * generate_before_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_main_content' );
+
+			if ( have_posts() ) :
+
+				/**
+				 * generate_archive_title hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_archive_title - 10
+				 */
+				do_action( 'generate_archive_title' );
+
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'siw_event' );
+				endwhile;
+
+				/**
+				 * generate_after_loop hook.
+				 *
+				 * @since 2.3
+				 */
+				do_action( 'generate_after_loop' );
+
+				generate_content_nav( 'nav-below' );
+
+			else :
+
+				get_template_part( 'no-results', 'archive' );
+
+			endif;
+
+			/**
+			 * generate_after_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_after_main_content' );
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
+get_footer();
---
+++ content-single-siw_event.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The template for displaying single posts.
+ *
+ * @copyright 2020 SIW Internationale Vrijwilligersprojecten
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+	<div class="inside-article">
+		<?php
+		/**
+		 * generate_before_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_featured_page_header_inside_single - 10
+		 */
+		do_action( 'generate_before_content' );
+		?>
+
+		<header class="entry-header">
+			<?php
+			/**
+			 * generate_before_entry_title hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_entry_title' );
+
+			if ( generate_show_title() ) {
+				the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
+			}
+
+			/**
+			 * generate_after_entry_title hook.
+			 *
+			 * @since 0.1
+			 *
+			 * @hooked generate_post_meta - 10
+			 */
+			do_action( 'generate_after_entry_title' );
+			?>
+		</header><!-- .entry-header -->
+
+		<?php
+		/**
+		 * generate_after_entry_header hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_post_image - 10
+		 */
+		do_action( 'generate_after_entry_header' );
+		?>
+
+		<div class="entry-content" itemprop="text">
+			<?php
+				do_action( 'siw_event_content' );
+			?>
+		</div><!-- .entry-content -->
+
+		<?php
+		/**
+		 * generate_after_entry_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_footer_meta - 10
+		 */
+		do_action( 'generate_after_entry_content' );
+
+		/**
+		 * generate_after_content hook.
+		 *
+		 * @since 0.1
+		 */
+		do_action( 'generate_after_content' );
+		?>
+	</div><!-- .inside-article -->
+</article><!-- #post-## -->
